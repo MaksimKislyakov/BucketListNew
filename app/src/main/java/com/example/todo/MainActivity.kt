151ffdbe0f924b68961dd26d79d1a093ede8377a
MaksimKislyakov <kislakov.maksim.vyacheslavovich@gmail.com>
2024-05-10 16:59:23 +0500

    save5_before_edit_gradle

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.example.todo
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val btnNavView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val btnNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         btnNavView.setupWithNavController(navController)
+
+
+
     }
-
-
 }
